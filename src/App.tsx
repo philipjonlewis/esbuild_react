75aba8c9ade31c82dd531a2c86eb7d72fc5847c1
Philip Lewis <philipjonlewis@gmail.com>
2022-05-03 16:28:17 +0800

    made some changes to the navbar scss

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,44 @@ import "./styles/style.scss";
 
 const app = () => {
   return (
-    <div className="">
-      <Header />
-      <h1 className="text-blue">itigil na natin tong mga to kabayan</h1>
-      <button className="btn-complement-light-yellow p-2 br-none">Hello</button>
+    <div>
+      <div className="navbar mb-r justify-space-between">
+        <div className="container">
+          <h2 className="site-title">Shinobi CSS</h2>
+          <p>a Lightweight sass Library for Devs</p>
+        </div>
+      </div>
+      <div className="container responsive-test">
+        <h2 className="mb-2">Grid System</h2>
+        <div className="row gap-2 justify-center">
+          <div className="col-xs-12 col-sm-4 col-xl-3">
+            <div className="card">
+              <h3 className="card-title">Hello Ninjas</h3>
+              <p className="card-body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-xs-12 col-sm-4 col-xl-3">
+            <div className="card">
+              <h3 className="card-title">Hello Ninjas</h3>
+              <p className="card-body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-xs-12 col-sm-4 col-xl-3">
+            <div className="card">
+              <h3 className="card-title">Hello Ninjas</h3>
+              <p className="card-body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
